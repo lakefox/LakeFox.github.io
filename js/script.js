6d4d86542edfb422b81e6c8c43ed4b8067d1e985
LakeFox <mason@lakefox.net>
2021-05-28 16:42:46 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+window.onload = () => {
+  let size = (document.querySelector(".header").getBoundingClientRect().height+500);
+  if (size > window.innerHeight-200) {
+    size = window.innerHeight-200;
+  }
+
+  document.querySelector(".main").style.top = size+"px";
+  if (window.innerWidth < 400) {
+    document.querySelector(".header").style.fontSize = "13em";
+  } else {
+    document.querySelector(".header").style.fontSize = "30em";
+  }
+}
+
+window.onresize = () => {
+  let size = (document.querySelector(".header").getBoundingClientRect().height+500);
+  if (size > window.innerHeight-200) {
+    size = window.innerHeight-200;
+  }
+
+  document.querySelector(".main").style.top = size+"px";
+
+  if (window.innerWidth < 400) {
+    document.querySelector(".header").style.fontSize = "13em";
+  } else {
+    document.querySelector(".header").style.fontSize = "30em";
+  }
+}
+
+window.onscroll = (e) => {
+  document.querySelector(".header").style.opacity = 1-(window.pageYOffset/window.innerHeight+0.2);
+}
